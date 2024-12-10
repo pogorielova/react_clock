5e49ca2c91f553cb80dd43b276acb06adf31d63c
pogorielova <nst.pogorielova@gmail.com>
2024-12-10 14:27:29 +0200

    upd

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -27,13 +27,17 @@ export class Clock extends React.PureComponent<Props, State> {
 
   componentDidMount(): void {
     this.nameTimerId = window.setInterval(() => {
-      this.setState({ name: getRandomName() });
+      this.setState({
+        name: getRandomName(),
+      });
     }, 3300);
 
     this.clockTimerId = window.setInterval(() => {
+      const currentTime = new Date();
+
       // eslint-disable-next-line no-console
-      console.log(this.state.today?.toUTCString().slice(-12, -4));
-      this.setState({ today: new Date() });
+      console.log(currentTime.toUTCString().slice(-12, -4));
+      this.setState({ today: currentTime });
     }, 1000);
   }
 
@@ -43,7 +47,7 @@ export class Clock extends React.PureComponent<Props, State> {
   ): void {
     if (prevState.name !== this.state.name) {
       // eslint-disable-next-line no-console
-      console.log(`Renamed from ${prevState.name} to ${this.state.name}`);
+      console.warn(`Renamed from ${prevState.name} to ${this.state.name}`);
     }
   }
 
